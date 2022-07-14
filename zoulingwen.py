5da9a2392963dc744d84528bef6abd1ca193d69a
ZZZZZlw <93489340@qq.com>
2022-07-14 17:41:34 +0800

    zoulingwen.py

--- zoulingwen.py
+++ zoulingwen.py
@@ -1,22 +1,24 @@
-#This python file uses the following encoding:utf-8
-#约瑟夫环问题：已知n个人（以编号1，2，3…n分别表示）围坐在一张圆桌周围。
-#从编号为k的人开始报数，数到k的那个人被杀掉；他的下一个人又从1开始报数，数到k的那个人又被杀掉；依此规律重复下去，直到圆桌周围的人只剩最后一个。
-
 from collections import deque
 
-total_num = int(input("> The total number is: "))
-k = int(input("> The target number is: "))
+def init_list():
 
-def surviver(total_num,k):
-    
+    global lucky_one
+    total_num = int(input("> The total number is: "))
+    offset = int(input("> The target number is: "))
     lucky_one = deque(range(1,total_num+1))   #总共有n个人参与
-    
+
+def survivor(total_num,offset):
+  
     while len(lucky_one) > 1:
     
-        lucky_one.rotate(-(k-1))  #循环左移k-1次，即到第k个人
+        lucky_one.rotate(-(offset-1))  #将左边元素放入右边，即左移k-1次，到第k个人 ； .rotate(1)默认将末尾放入左边
         lucky_one.popleft()       #删除最左端的第k个元素
     
-    return lucky_one[0]  
+    return lucky_one[0] 
     
+    
+print("So the last one is: " , survivor(total_num,offset)) 
 
-print( "So the last one is: " , surviver (total_num,k))
+init_list()
+survivor()
+
